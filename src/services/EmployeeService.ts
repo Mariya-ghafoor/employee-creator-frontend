34458ec3ac94428798b04863a5d16fe9dc673d99
Mariya Abdul Ghafoor <mariya.aghafoor@gmail.com>
2024-01-02 11:00:18 +1100

    adds live API links

--- src/services/EmployeeService.ts
+++ src/services/EmployeeService.ts
@@ -5,55 +5,65 @@ export interface Employee {
   email: string;
   type: string;
 }
-export const getEmployees = async(): Promise<Employee[]> => {
-  const response = await fetch("http://localhost:8080/employees");
+export const getEmployees = async (): Promise<Employee[]> => {
+  const response = await fetch(
+    "https://employee-creator-backend-cxqr.onrender.com/employees"
+  );
   const data = await response.json();
-  console.log("all employees data is "+data);
+  console.log("all employees data is " + data);
   return data;
+};
 
-}
-
-export const getEmployeeById = async(employeeId: string): Promise<Employee> => {
-  const response = await fetch(`http://localhost:8080/employees/${employeeId}`);
-  const data = await response.json()
+export const getEmployeeById = async (
+  employeeId: string
+): Promise<Employee> => {
+  const response = await fetch(
+    `https://employee-creator-backend-cxqr.onrender.com/employees/${employeeId}`
+  );
+  const data = await response.json();
   // console.log("employee data: ",data)
   return data;
+};
 
-}
+export const addEmployee = async (data: unknown): Promise<unknown> => {
+  const response = await fetch(
+    "https://employee-creator-backend-cxqr.onrender.com/employees",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    }
+  );
+  return response.json();
+};
 
-export const addEmployee = async(data:unknown):Promise<unknown> => {
+export const deleteEmployee = async (id: number): Promise<unknown> => {
+  const response = await fetch(
+    `https://employee-creator-backend-cxqr.onrender.com/employees/${id}`,
+    {
+      method: "DELETE",
+    }
+  );
+  console.log("response is ", response.ok);
+  return response.ok;
+};
 
-  const response = await fetch("http://localhost:8080/employees", {
-    method: "POST", 
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  });
-  return response.json(); 
+export const editEmployee = async (data: Employee): Promise<Employee> => {
+  console.log("****data receieved in services " + data.type);
 
-}
-
-export const deleteEmployee = async(id:number):Promise<unknown> => {
-  const response = await fetch(`http://localhost:8080/employees/${id}`, {
-    method: "DELETE",
-  });
-  console.log('response is ',response.ok)
-  return response.ok; 
-}
-
-export const editEmployee = async(data:Employee):Promise<Employee> => {
-  console.log("****data receieved in services "+data.type)
-
-  const response = await fetch(`http://localhost:8080/employees/${data.id}`, {
-    method: "PATCH", 
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data), 
-  });
+  const response = await fetch(
+    `https://employee-creator-backend-cxqr.onrender.com/employees/${data.id}`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }
+  );
   const dataReceieved = await response.json();
-  console.log("## response from server "+dataReceieved.type)
-  return dataReceieved; 
-
-}
+  console.log("## response from server " + dataReceieved.type);
+  return dataReceieved;
+};
